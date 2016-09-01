733ccd92931a000d252ef51b22447801b98c1fdc
hejiaxin <13727349175@163.com>
2016-09-01 09:20:24 +0800

    内容

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(function(){
+    		$("#fullpage").fullpage({
+    			 "verticalCentered": false,
+		         "css3": true,
+		         "sectionsColor": ["#4CB2B7", "#336699", "#695684"],
+		          anchors: ["page1", "page2", "page3"],
+		         "navigation": true,
+		         "navigationPosition": "right",
+		         "navigationTooltips": ["首页", "专业技能", "感谢"],
+		          afterLoad: function(anchorLink, index){
+		          	if(index==1){
+		          		$(".myself").fadeIn(3000);
+		          	}
+		          	else if(index==2){
+		    			$(".skill").fadeIn(3000);
+		    			$(".firstSkill").addClass("a");		
+		          	}
+		          	else {
+		    			$(".end").fadeIn(3000);		          		
+		          	}
+		         }
+    		});
+        });
